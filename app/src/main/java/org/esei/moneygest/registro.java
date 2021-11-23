b2aee862a0d826d03f804994a2280eef16826c88
DiegoRT99 <diegorodrigueztaboada99@gmail.com>
2021-11-23 21:12:30 +0100

    Cambios para que desde registro se pueda volver a Login

--- app/src/main/java/org/esei/moneygest/registro.java
+++ app/src/main/java/org/esei/moneygest/registro.java
@@ -1,7 +1,9 @@
 package org.esei.moneygest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class registro extends Activity {
 
@@ -9,5 +11,13 @@ public class registro extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registro);
+    }
+
+    public void ejecutar_login(View view){
+
+        Intent i= new Intent(this, MainActivity.class);
+
+        startActivity(i);
+
     }
 }
