d2091f441efe6b8be6480465e531db318e6e6730
Michael Sordo <michaelsordoseijo@gmail.com>
2021-11-23 19:12:19 +0100

    add login connect registro

--- app/src/main/java/org/esei/moneygest/MainActivity.java
+++ app/src/main/java/org/esei/moneygest/MainActivity.java
@@ -2,7 +2,9 @@ package org.esei.moneygest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
     }
+
+    public void ejecutar_registro(View view){
+
+        Intent i= new Intent(this, registro.class);
+
+        startActivity(i);
+
+    }
+
 }
---
+++ app/src/main/java/org/esei/moneygest/registro.java
@@ -0,0 +1,13 @@
+package org.esei.moneygest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class registro extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+    }
+}
