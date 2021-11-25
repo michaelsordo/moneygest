a7f8dac2c0f11af5b8455318034128448f94836a
Michael Sordo <michaelsordoseijo@gmail.com>
2021-11-25 18:05:05 +0100

    Rename layouts Add activity_ name

--- app/src/main/java/org/esei/moneygest/login.java
+++ app/src/main/java/org/esei/moneygest/login.java
@@ -8,6 +8,6 @@ public class login extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/org/esei/moneygest/registro.java
+++ app/src/main/java/org/esei/moneygest/registro.java
@@ -10,7 +10,7 @@ public class registro extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registro);
+        setContentView(R.layout.activity_registro);
     }
 
     public void ejecutar_login(View view){
