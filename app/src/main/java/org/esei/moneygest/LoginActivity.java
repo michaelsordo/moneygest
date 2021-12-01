34908cdf5265c71f0f19a6fd10bf353626605c6f
DiegoRT99 <diegorodrigueztaboada99@gmail.com>
2021-12-01 20:25:41 +0100

    Primera Implementación de SharedPreferences

--- app/src/main/java/org/esei/moneygest/LoginActivity.java
+++ app/src/main/java/org/esei/moneygest/LoginActivity.java
@@ -2,7 +2,9 @@ package org.esei.moneygest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +21,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        cargar_preferencias();
 
 
         /*
@@ -81,6 +85,7 @@ public class LoginActivity extends AppCompatActivity {
         else{
             Boolean checkuserpass = DB.checkUsernamePass(user, pass);
             if(checkuserpass){
+                guardar_preferencias();
                 Toast.makeText(LoginActivity.this, "Usuario logueado correctamente", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(intent);
@@ -91,6 +96,35 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    private void guardar_preferencias(){
+        SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+
+        username = (EditText) findViewById(R.id.login_nombre_usuario);
+        password = (EditText) findViewById(R.id.login_pass_usuario);
+
+        String usuario = username.getText().toString();
+        String pass = password.getText().toString();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("user", usuario);
+        editor.putString("pass", pass);
+
+        editor.commit();
+    }
+
+    private void cargar_preferencias(){
+        SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+
+        username = (EditText) findViewById(R.id.login_nombre_usuario);
+        password = (EditText) findViewById(R.id.login_pass_usuario);
+
+        String usuario = preferences.getString("user", "");
+        String pass = preferences.getString("pass", "");
+
+        username.setText(usuario);
+        password.setText(pass);
     }
 
 
