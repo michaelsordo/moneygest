317021bbc46f93a9c3588e81f2cf0b7cd1c74355
Michael Sordo <michaelsordoseijo@gmail.com>
2021-12-02 11:13:36 +0100

    Add menu a pantalla gastos

--- app/src/main/java/org/esei/moneygest/GastosActivity.java
+++ app/src/main/java/org/esei/moneygest/GastosActivity.java
@@ -3,14 +3,74 @@ package org.esei.moneygest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-public class GastosActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GastosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gastos_general);
+    }
+
+    //MENU
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        super.onCreateOptionsMenu(menu);
+        this.getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        boolean toret=false;
+        Intent intent;
+
+        switch (item.getItemId()){
+
+            case R.id.item0:
+                intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+                toret=true;
+                break;
+
+            case R.id.item1:
+                intent = new Intent(getApplicationContext(), GastosActivity.class);
+                startActivity(intent);
+                toret=true;
+                break;
+
+            case R.id.item2:
+                intent = new Intent(getApplicationContext(),IngresosActivity.class);
+                startActivity(intent);
+                toret=true;
+                break;
+
+            case R.id.item3:
+                intent = new Intent(getApplicationContext(),AreaPersonalActivity.class);
+                startActivity(intent);
+                toret=true;
+                break;
+
+            case R.id.item4:
+                UtilidadesSP utilidadesSP = new UtilidadesSP();
+                utilidadesSP.logout(GastosActivity.this);
+                Toast.makeText(GastosActivity.this, "Sesión cerrada correctamente", Toast.LENGTH_LONG).show();
+                intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                toret=true;
+                break;
+        }
+
+        return toret;
     }
 
 
