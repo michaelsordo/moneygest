338eb26e9916a33c4fd00a4994533427fcc2ee94
Michael Sordo <michaelsordoseijo@gmail.com>
2021-12-06 12:42:26 +0100

    Add layout edit gastos, todavía en pruebas

--- app/src/main/java/org/esei/moneygest/GastosEditActivity.java
+++ app/src/main/java/org/esei/moneygest/GastosEditActivity.java
@@ -1,4 +1,24 @@
 package org.esei.moneygest;
 
-public class GastosEditActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.esei.moneygest.model.Gasto;
+import org.esei.moneygest.model.GastoMapper;
+
+import java.util.ArrayList;
+
+public class GastosEditActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_gastos);
+
+    }
 }
