e67e470186d203ad22fefcf46fbd775017904016
Michael Sordo <michaelsordoseijo@gmail.com>
2021-12-07 02:23:50 +0100

    Cambios en filtros activity, propiedad gone para los edit text

--- app/src/main/java/org/esei/moneygest/FiltrosActivity.java
+++ app/src/main/java/org/esei/moneygest/FiltrosActivity.java
@@ -1,6 +1,10 @@
 package org.esei.moneygest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +16,28 @@ import java.util.ArrayList;
 
 public class FiltrosActivity extends AppCompatActivity {
 
+CheckBox cbCantidadGastos;
+EditText etCantidad;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtros);
+
+        cbCantidadGastos = findViewById(R.id.checkBox_cantidad_gastos);
+        etCantidad = findViewById(R.id.cantidad);
+
+        cbCantidadGastos.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean esta_pulsado) {
+                if(esta_pulsado){
+                    etCantidad.setVisibility(View.VISIBLE);
+                }else{
+                    etCantidad.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
     }
 }
